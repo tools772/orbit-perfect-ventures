09d8204d01f9eb8396a371311fb3b9838fe16acb
Anirudhmx <anirudhmx@gmail.com>
2026-08-06 14:09:21 +0530

    feature updates

--- packages/twenty-server/src/engine/core-modules/onboarding/jobs/install-onboarding-apps.job-constants.ts
+++
@@ -1,6 +0,0 @@
-export const INSTALL_ONBOARDING_APPS_JOB_NAME = 'InstallOnboardingAppsJob';
-
-export type InstallOnboardingAppsJobData = {
-  workspaceId: string;
-  universalIdentifiers: string[];
-};
--- packages/twenty-server/src/engine/metadata-modules/flat-entity/constant/all-metadata-side-effect-companion-metadata-names.constant.ts
+++
@@ -1,8 +0,0 @@
-import { type AllMetadataName } from 'twenty-shared/metadata';
-
-export const ALL_METADATA_SIDE_EFFECT_COMPANION_METADATA_NAMES = {
-  fieldMetadata: ['index', 'searchFieldMetadata'],
-  objectMetadata: ['fieldMetadata', 'index', 'searchFieldMetadata'],
-} as const satisfies Partial<
-  Record<AllMetadataName, readonly AllMetadataName[]>
->;
